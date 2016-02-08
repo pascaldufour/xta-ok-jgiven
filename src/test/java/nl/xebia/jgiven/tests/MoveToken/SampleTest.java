093a6440cba2280105ca5ce069155571c5f93dcd
Arjan Molenaar <gaphor@gmail.com>
2016-02-08 22:20:09 +0100

    Strip unit test

--- src/test/java/nl/xebia/jgiven/tests/MoveToken/SampleTest.java
+++ src/test/java/nl/xebia/jgiven/tests/MoveToken/SampleTest.java
@@ -6,14 +6,15 @@ import steps.GivenGameState;
 import steps.ThenGameOutcome;
 import steps.WhenGameAction;
 
+import static org.junit.Assert.fail;
+
 public class SampleTest extends
         ScenarioTest<GivenGameState, WhenGameAction, ThenGameOutcome> {
 
     @Test
     public void move_token() {
-        given().the_players_token_is_at_the_start_position();
-        when().the_player_throws_both_dice();
-        then().the_player_moves_to_a_new_position();
+        given();
+        fail("Implement me!");
     }
 
 }
